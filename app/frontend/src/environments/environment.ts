939b51c0235d84f63153ae0a84beadb8e63e45db
yufrances90 <yufrances90@gmail.com>
2020-04-05 12:20:08 -0700

    chore: Added Auth0 configuration

--- app/frontend/src/environments/environment.ts
+++ app/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: process.env.AUTH0_URL, // the auth0 domain prefix
+    audience: process.env.AUTH0_AUDIENCE, // the audience set for the auth0 app
+    clientId: process.env.AUTH0_CLIENT_ID, // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
