b5af5e5af35ee260415dfe88ed72dc7015ecf826
yufrances90 <yufrances90@gmail.com>
2020-04-07 09:53:14 -0700

    fix: Fixed formatting issues

--- app/backend/src/auth/auth.py
+++ app/backend/src/auth/auth.py
@@ -8,22 +8,16 @@ from ..exceptions.error import AuthError
 
 AUTH_CONFIG = get_auth_config()
 
-## Auth Header
+# Auth Header
 
-'''
-    it should attempt to get the header from the request
-        it should raise an AuthError if no header is present
-    it should attempt to split bearer and the token
-        it should raise an AuthError if the header is malformed
-    return the token part of the header
-'''
+
 def get_token_auth_header():
 
     request_headers = request.headers
 
     if request_headers is None:
         raise AuthError({
-            'code': 'invalid_header', \
+            'code': 'invalid_header',
             'description': "No header is present"
         }, 401)
 
@@ -33,7 +27,7 @@ def get_token_auth_header():
 
     if "Bearer" not in auth_arr or len(auth_arr) != 2:
         raise AuthError({
-            'code': 'invalid_header', \
+            'code': 'invalid_header',
             'description': "The header is malformed"
         }, 401)
 
@@ -41,16 +35,7 @@ def get_token_auth_header():
 
     return token
 
-'''
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        payload: decoded jwt payload
 
-    it should raise an AuthError if permissions are not included in the payload
-        !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
-    return true otherwise
-'''
 def check_permissions(permission, payload):
 
     if 'permissions' not in payload:
@@ -69,15 +54,7 @@ def check_permissions(permission, payload):
 
     return True
 
-'''
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
 
-    it should use the get_token_auth_header method to get the token
-    it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
-'''
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
